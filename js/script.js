690d9b4d59c6851d45fc9b168be9339f80f21cf6
lukemagalhaes <lukemmagalhaes@gmail.com>
2022-09-27 17:13:30 -0300

    modified:   css/header.css 	modified:   css/technology.css 	modified:   documento.docx 	modified:   js/script.js 	modified:   technology.html

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+var technologyTitle = document.getElementById("technologyTitle");
+var technologyParagraph = document.getElementById("technologyParagraph");
+var technologyImg = document.getElementById("technologyImg");
+
+function technologyBtn1() {
+  technologyTitle.textContent = "nada1";
+  technologyParagraph.textContent = "nada";
+  technologyImg.src = "/assets/technology/image-space-capsule-portrait.jpg";
+}
+
+function technologyBtn2() {
+  technologyTitle.textContent = "nada2";
+  technologyParagraph.textContent = "nada";
+  technologyImg.textContent = "nada";
+}
+
+function technologyBtn3() {
+  technologyTitle.textContent = "nada3";
+  technologyParagraph.textContent = "nada";
+  technologyImg.textContent = "nada";
+}
